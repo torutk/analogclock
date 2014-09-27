5194b17a5df3bd43173462e5fd3ac65de7595151
TAKAHASHI, Toru <torutk@gmail.com>
2014-09-27 19:41:09 +0900

    マウスドラッグ＆ドロップ操作で時計表示位置を移動できるようにした。

--- AnalogClockSvg/src/analogclocksvg/AnalogClockSvg.java
+++ AnalogClockSvg/src/analogclocksvg/AnalogClockSvg.java
@@ -7,7 +7,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -15,12 +17,23 @@ import javafx.stage.Stage;
  */
 public class AnalogClockSvg extends Application {
 
+    private double dragStartX;
+    private double dragStartY;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("AnalogClock.fxml"));
 
-        Scene scene = new Scene(root);
-
+        Scene scene = new Scene(root, 200, 200, Color.TRANSPARENT);
+        scene.setOnMousePressed(e -> {
+            dragStartX = e.getSceneX();
+            dragStartY = e.getSceneY();
+        });
+        scene.setOnMouseDragged(e -> {
+            stage.setX(e.getScreenX() - dragStartX);
+            stage.setY(e.getScreenY() - dragStartY);
+        });
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
