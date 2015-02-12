317753300aee070e0be1838361d13395e4e3d9b7
TAKAHASHI, Toru <torutk@gmail.com>
2015-02-13 01:42:39 +0900

    右クリックでコンテクストメニューを表示し、exit項目を設けて終了できるようにした。

--- AnalogClockSvg/src/analogclocksvg/AnalogClockSvg.java
+++ AnalogClockSvg/src/analogclocksvg/AnalogClockSvg.java
@@ -4,9 +4,13 @@
 package analogclocksvg;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,6 +23,7 @@ public class AnalogClockSvg extends Application {
 
     private double dragStartX;
     private double dragStartY;
+    private ContextMenu popup = new ContextMenu();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -33,6 +38,16 @@ public class AnalogClockSvg extends Application {
             stage.setX(e.getScreenX() - dragStartX);
             stage.setY(e.getScreenY() - dragStartY);
         });
+
+        MenuItem exitItem = new MenuItem("exit");
+        exitItem.setOnAction(e -> Platform.exit());
+        popup.getItems().add(exitItem);
+        scene.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.SECONDARY) {
+                popup.show(stage, e.getScreenX(), e.getScreenY());
+            }
+        });
+
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
